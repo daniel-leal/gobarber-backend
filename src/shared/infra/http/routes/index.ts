ad198e313a836b29085bd2df0c1d9d3fbe6d1bd9
daniel-leal <danielleal94@gmail.com>
2020-06-11 01:28:00 -0300

    template de email

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@modules/users/infra/http/routes/user.routes';
@@ -6,6 +6,10 @@ import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
+
+routes.get('/', (req, res) => {
+  return res.json({ status: 'ok', app: 'gobarber' })
+})
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
